4934aa68db88f030529e69f8d9c681152ba528f0
wzj97 <841424574@qq.com>
2019-07-10 17:59:49 +0800

    第一版

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+   <mt-header fixed title="吴子金最牛逼"></mt-header>
+
+
+
+
+
+   <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a><a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">会员</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">购物车</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">搜索</span>
+			</a>
+		</nav>
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
